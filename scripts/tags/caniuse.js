eda062436a82f590991c43af8baf2236a605c8e7
wangjiezhe <wangjiezhe@gmail.com>
2025-10-19 03:48:35 +0800

    Use my own caniuse api

--- scripts/tags/caniuse.js
+++ scripts/tags/caniuse.js
@@ -12,5 +12,5 @@ module.exports = ctx => function(args) {
     return '';
   }
 
-  return `<iframe data-feature="${feature}" src="https://caniuse.bitsofco.de/embed/index.html?feat=${feature}&periods=${periods}&accessible-colours=false" frameborder="0" width="100%" height="400px"></iframe>`;
+  return `<iframe data-feature="${feature}" src="https://caniuse.wangjiezhe.com/embed/index.html?feat=${feature}&periods=${periods}&accessible-colours=false" frameborder="0" width="100%" height="400px"></iframe>`;
 };
--- test/tags/caniuse.js
+++ test/tags/caniuse.js
@@ -7,10 +7,10 @@ describe('caniuse', () => {
   const caniUse = require('../../scripts/tags/caniuse')(hexo);
 
   it('only feature', () => {
-    caniUse(['loading-lazy-attr']).should.eql('<iframe data-feature="loading-lazy-attr" src="https://caniuse.bitsofco.de/embed/index.html?feat=loading-lazy-attr&periods=current&accessible-colours=false" frameborder="0" width="100%" height="400px"></iframe>');
+    caniUse(['loading-lazy-attr']).should.eql('<iframe data-feature="loading-lazy-attr" src="https://caniuse.wangjiezhe.com/embed/index.html?feat=loading-lazy-attr&periods=current&accessible-colours=false" frameborder="0" width="100%" height="400px"></iframe>');
   });
 
   it('feature and periods', () => {
-    caniUse('fetch @ future_3,future_2,future_1'.split(' ')).should.eql('<iframe data-feature="fetch" src="https://caniuse.bitsofco.de/embed/index.html?feat=fetch&periods=future_3,future_2,future_1&accessible-colours=false" frameborder="0" width="100%" height="400px"></iframe>');
+    caniUse('fetch @ future_3,future_2,future_1'.split(' ')).should.eql('<iframe data-feature="fetch" src="https://caniuse.wangjiezhe.com/embed/index.html?feat=fetch&periods=future_3,future_2,future_1&accessible-colours=false" frameborder="0" width="100%" height="400px"></iframe>');
   });
 });
